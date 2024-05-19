12af160177337250d79aba827657489e3230ba32
jadon6 <uwizeye.jean@wiredin.rw>
2024-05-19 18:58:44 +0200

    add user on saving

--- core/api/views.py
+++ core/api/views.py
@@ -7,8 +7,14 @@ class LeaveRequesCreateAPIView(generics.CreateAPIView):
     queryset = LeaveRequest.objects.all()
     permission_classes = [IsAuthenticated]
     serializer_class = LeaveRequestSerializer
+    
+    def perform_create(self, serializer):
+        serializer.save(requested_by=self.request.user)
 
 class SiteIssueCreateAPIView(generics.CreateAPIView):
     queryset = SiteIssue.objects.all()
     permission_classes = [IsAuthenticated]
     serializer_class = SiteIssueSerializer
+    
+    def perform_create(self, serializer):
+        serializer.save(reported_by=self.request.user)
